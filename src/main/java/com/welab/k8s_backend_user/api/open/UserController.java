620b397b0936cd414a56037831acad3c78bffd88
seoyoun8694 <chaeseoyoun8694@gmail.com>
2025-06-11 14:18:11 +0900

    .

--- src/main/java/com/welab/k8s_backend_user/api/open/UserController.java
+++ src/main/java/com/welab/k8s_backend_user/api/open/UserController.java
@@ -1,5 +1,6 @@
 package com.welab.k8s_backend_user.api.open;
 
+import com.welab.k8s_backend_user.common.dto.ApiResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -7,11 +8,10 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping(value = "/api/user/v1", produces = MediaType.APPLICATION_JSON_VALUE)
-@RequiredArgsConstructor
+@RequestMapping(value = "/api/user/v1", produces = MediaType.APPLICATION_JSON_VALUE)@RequiredArgsConstructor
 public class UserController {
-    @GetMapping(value = "/test")
-    public String test() {
-        return "Hello Kubernetes";
+    @GetMapping(value = "/hello")
+    public ApiResponseDto<String> hello() {
+        return ApiResponseDto.createOk("웰컴 투 백엔드 유저");
     }
 }
