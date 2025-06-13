c66961de11a0e7478fbd63e09df9cabc2a2b06a2
seoyoun8694 <chaeseoyoun8694@gmail.com>
2025-06-13 10:17:36 +0900

    .

--- src/main/java/com/welab/k8s_backend_user/api/open/UserController.java
+++ src/main/java/com/welab/k8s_backend_user/api/open/UserController.java
@@ -1,7 +1,10 @@
 package com.welab.k8s_backend_user.api.open;
 
 import com.welab.k8s_backend_user.common.dto.ApiResponseDto;
+import com.welab.k8s_backend_user.domain.dto.SiteUserRegisterDto;
 import com.welab.k8s_backend_user.remote.alim.RemoteAlimService;
+import com.welab.k8s_backend_user.service.SiteUserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -9,12 +12,22 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping(value = "/api/user/v1", produces = MediaType.APPLICATION_JSON_VALUE)@RequiredArgsConstructor
+@RequestMapping(value = "/api/user/v1", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequiredArgsConstructor
 public class UserController {
+    private final SiteUserService siteUserService;
     private final RemoteAlimService remoteAlimService;
-    @GetMapping(value = "/hello")
-    public ApiResponseDto<String> hello() {
+
+    @GetMapping(value = "/test")
+    public String test() {
         String remoteMessage = remoteAlimService.hello().getData();
-        String userResponse = "웰컴 투 백엔드 유저. 리모트 알림 메시지= " + remoteMessage;return ApiResponseDto.createOk(userResponse);
+
+        return "웰컴 투 유저 서비스: 리모트 메시지 = " + remoteMessage;
+    }
+
+    @PostMapping(value = "/register")
+    public ApiResponseDto<String> register(@RequestBody @Valid SiteUserRegisterDto registerDto) {
+        siteUserService.registerUser(registerDto);
+        return ApiResponseDto.defaultOk();
     }
 }
